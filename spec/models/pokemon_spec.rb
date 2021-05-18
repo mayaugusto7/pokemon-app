8dc14e4ef67bc88bad09e1c33a3031f6010387c1
Maycon Augusto <mayaugusto777@gmail.com>
2021-05-18 10:41:54 -0300

    Pokemon test
    Signed-off-by: Maycon Augusto <mayaugusto777@gmail.com>

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,2 +1,21 @@
-class PokemonSpec
+require 'rails_helper'
+
+RSpec.describe Pokemon, type: :model do
+  describe '#nome_completo' do
+    context 'quando possui nome e o id nacional' do
+      let(:pokemon) do
+        Pokemon.new(nome: 'Charizard', id_nacional: 6)
+      end
+      it 'exibe o nome e o id nacional' do
+        expect(pokemon.nome_completo).to eq('Charizard - 6')
+      end
+    end
+
+    context 'quando não possui o nome e o id nacional' do
+      it 'é nil' do
+        pokemon = Pokemon.new
+        expect(pokemon.nome_completo).to be_nil
+      end
+    end
+  end
 end
